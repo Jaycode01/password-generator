28b7a8dbab6d498d5e9232c6da53b3a858b2ab43
Jaycode01 <josephlamidi01@gmail.com>
2024-10-31 06:17:26 +0100

    Initial

---
+++ script.js
@@ -0,0 +1,46 @@
+// //Let's go
+// /* Code
+// by
+// Jaycode*/
+// function passwordGenerator(length, includeLowercase, includeLUppercase, includeNumber, includeSymbols){
+//     const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+//     const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+//     const numberChars = '0123456789';
+//     const symbolsChars = '~!@#$%^&*()_|+*-/'
+
+
+//     let allowedChars = '';
+//     const password = '';
+
+//     allowedChars += includeLowercase ? lowercaseChars : '';
+//     allowedChars += includeLUppercase ? uppercaseChars : '';
+//     allowedChars += includeNumber ? numberChars : '' ;
+//     allowedChars += includeSymbols ? symbolsChars : '' ;
+
+// if(length <= 0){
+//     return `(Password must be at least 1)`
+// }
+
+// if(allowedChars.length === 0){
+//     return `(At least one set of character needs to be selected)`
+// }
+
+// for(let i = 0; i < length; i++){
+//     const randomIndex = Math.floor(Math.random() * allowedChars.length);
+//     password += allowedChars;[random]
+// }
+
+//     return  password;
+// }
+
+
+
+// const passwordLength = 12;
+// const includeLowercase = true;
+// const includeLUppercase = true;
+// const includeNumber = true;
+// const includeSymbols = true;
+
+// const password = passwordGenerator(passwordLength, includeLowercase,  includeLUppercase, includeNumber,  includeSymbols);
+
+// console.log('Generated Password: ${password}');
