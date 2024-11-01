f30f4787bdb1007e4f2317179597a2ffb50b93ea
Jaycode01 <josephlamidi01@gmail.com>
2024-11-01 23:31:00 +0100

    ...

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
-// //Let's go
-// /* Code
-// by
-// Jaycode*/
-// function passwordGenerator(length, includeLowercase, includeLUppercase, includeNumber, includeSymbols){
-//     const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
-//     const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-//     const numberChars = '0123456789';
-//     const symbolsChars = '~!@#$%^&*()_|+*-/'
+//Let's go
+/* Code
+by
+Jaycode*/
+function passwordGenerator(length, includeLowercase, includeLUppercase, includeNumber, includeSymbols){
+    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numberChars = '0123456789';
+    const symbolsChars = '~!@#$%^&*()_|+*-/'
 
 
-//     let allowedChars = '';
-//     const password = '';
+    let allowedChars = '';
+    const password = '';
 
-//     allowedChars += includeLowercase ? lowercaseChars : '';
-//     allowedChars += includeLUppercase ? uppercaseChars : '';
-//     allowedChars += includeNumber ? numberChars : '' ;
-//     allowedChars += includeSymbols ? symbolsChars : '' ;
+    allowedChars += includeLowercase ? lowercaseChars : '';
+    allowedChars += includeLUppercase ? uppercaseChars : '';
+    allowedChars += includeNumber ? numberChars : '' ;
+    allowedChars += includeSymbols ? symbolsChars : '' ;
 
-// if(length <= 0){
-//     return `(Password must be at least 1)`
-// }
+if(length <= 0){
+    return `(Password must be at least 1)`
+}
 
-// if(allowedChars.length === 0){
-//     return `(At least one set of character needs to be selected)`
-// }
+if(allowedChars.length === 0){
+    return `(At least one set of character needs to be selected)`
+}
 
-// for(let i = 0; i < length; i++){
-//     const randomIndex = Math.floor(Math.random() * allowedChars.length);
-//     password += allowedChars;[random]
-// }
+for(let i = 0; i < length; i++){
+    const randomIndex = Math.floor(Math.random() * allowedChars.length);
+    password += allowedChars;
+}
 
-//     return  password;
-// }
+    return  password;
+}
 
+const passwordLength = 12;
+const includeLowercase = true;
+const includeLUppercase = true;
+const includeNumber = true;
+const includeSymbols = true;
 
+const password = passwordGenerator(passwordLength, includeLowercase,  includeLUppercase, includeNumber,  includeSymbols);
 
-// const passwordLength = 12;
-// const includeLowercase = true;
-// const includeLUppercase = true;
-// const includeNumber = true;
-// const includeSymbols = true;
-
-// const password = passwordGenerator(passwordLength, includeLowercase,  includeLUppercase, includeNumber,  includeSymbols);
-
-// console.log('Generated Password: ${password}');
+console.log('Generated Password: ${password}');
